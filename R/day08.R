eb3444b264ed63495580c754a3d4fb8e87f8e7d9
Andrea Gilardi <andrea.gilardi@unimib.it>
2022-12-13 22:44:06 +0100

    day08

--- R/day08.R
+++ R/day08.R
@@ -1,5 +1,5 @@
 # Read-in input
-input <- readLines("data/input08")
+input <- readLines("data/input08.txt")
 input <- do.call("rbind", strsplit(input, ""))
 input <- matrix(as.numeric(input), nrow = nrow(input))
 
@@ -19,3 +19,28 @@ for (i in seq.int(2, n - 1)) {
     }
   }
 }
+num_visible
+
+# Part 2
+compute_vd <- function(x) {
+  if (!any(x)) {
+    return(length(x))
+  }
+  which(x)[1L]
+}
+
+max_scenic_score <- 0L
+for (i in 2:(n - 1)) {
+  for (j in 2:(n - 1)) {
+    tree <- input[i, j]
+    vd_up <- compute_vd(tree <= input[(i - 1):1, j])
+    vd_down <- compute_vd(tree <= input[(i + 1):n, j])
+    vd_left <- compute_vd(tree <= input[i, (j - 1):1])
+    vd_right <- compute_vd(tree <= input[i, (j + 1):n])
+    scenic_score <- vd_up * vd_down * vd_left * vd_right
+    if (scenic_score > max_scenic_score) {
+      max_scenic_score = scenic_score
+    }
+  }
+}
+max_scenic_score
